4a98c7925a890797a287f4d2296fc5c0decab2bd
unknown <kariimgamal1997@gmail.com>
2023-02-19 00:57:20 +0200

    Adding Files after updating

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,12 @@ import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import ContactView from "../views/ContactView.vue";
-<<<<<<< HEAD
 import InstructionsView from "../views/InstructionsView.vue";
 import FaqView from "../views/FaqView.vue";
-
-=======
-import LoginView from '@/views/LoginView'
->>>>>>> 6997a016a8304bba64b66289d3a7e8bb05035019
+import LoginView from "@/views/LoginView";
 Vue.use(VueRouter);
 
 const routes = [
-  
   {
     path: "/",
     name: "home",
@@ -35,15 +30,14 @@ const routes = [
     component: ContactView,
   },
   {
-<<<<<<< HEAD
     path: "/faq",
     name: "faq",
     component: FaqView,
-=======
+  },
+  {
     path: "/login",
     name: "login",
     component: LoginView,
->>>>>>> 6997a016a8304bba64b66289d3a7e8bb05035019
   },
 ];
 
